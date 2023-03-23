22949f3d611b9d7d4fe00f9474a4c1720fb41a09
Raymond <macross@macrosss-MacBook-Pro.local>
2023-03-22 23:30:33 -0700

    added forward propagation for LSTM cell

--- flare/layers/rnn/lstm_cell.hpp
+++ flare/layers/rnn/lstm_cell.hpp
@@ -15,46 +15,164 @@ template<typename GateActivation = Sigmoid, typename RecurrentActivation = TanH>
 class LSTMCell
 {
 public:
-    LSTMCell(Eigen::Index input_len, Eigen::Index output_len, Eigen::Index time_step)
-            : time_step(time_step)
+    LSTMCell(Eigen::Index time_step, Eigen::Index input_len, Eigen::Index output_len)
+            : time_step(time_step),
+              input_len(input_len),
+              output_len(output_len)
     {
-
+        std::cout << "initialize cell: " << time_step << ", " << input_len << ", "
+                  << output_len << "\n";
     }
 
 
     template<typename Device>
-    void Forward(const Tensor<3> &x, Tensor<3> &h, const Tensor<3> &c_prev,
-                 const Device &device)
+    void Forward(const Tensor<3> &x, const Tensor<2> &w,
+                 Tensor<3> &h, Tensor<3> &cs,
+                 const Device &device = Eigen::DefaultDevice())
     {
+        // resize for multi-threading
         this->gates.resize(x.dimension(0), this->output_len * 4);
+        this->p_gates.resize(this->gates.dimensions());
+        this->x_h_prev.resize(x.dimension(0), this->input_len + this->output_len);
 
+        this->x_h_prev
+                .slice(Dims<2>(0, 0),
+                       Dims<2>(x.dimension(0), this->input_len))
+                .device(device) = x.chip(this->time_step, 1);
+
+        if (this->time_step != 0) {
+            this->x_h_prev
+                    .slice(Dims<2>(0, this->input_len),
+                           Dims<2>(x.dimension(0), this->output_len))
+                    .device(device) = h.chip(this->time_step - 1, 1);
+        }
+        else {
+            this->x_h_prev
+                    .slice(Dims<2>(0, this->input_len),
+                           Dims<2>(x.dimension(0), this->output_len))
+                    .setZero();
+        }
+
+        this->p_gates = this->x_h_prev.contract(w, ContractDim {Axes(1, 0)});
+
+        this->gates.slice(this->i_offset(), this->gate_extent()).device(device) =
+                GateActivation::Activate(
+                        this->p_gates.slice(this->i_offset(), this->gate_extent()));
+
+        this->gates.slice(this->f_offset(), this->gate_extent()).device(device) =
+                GateActivation::Activate(
+                        this->p_gates.slice(this->f_offset(), this->gate_extent()));
+
+        this->gates.slice(this->c_offset(), this->gate_extent()).device(device) =
+                RecurrentActivation::Activate(
+                        this->p_gates.slice(this->c_offset(), this->gate_extent()));
+
+        this->gates.slice(this->o_offset(), this->gate_extent()).device(device) =
+                GateActivation::Activate(
+                        this->p_gates.slice(this->o_offset(), this->gate_extent()));
+
+        cs.chip(this->time_step, 1).device(device) =
+                this->gates.slice(this->f_offset(), this->gate_extent()) *
+                (this->time_step == 0 ? Tensor<2>(1, 5).setZero() : cs.chip(this->time_step - 1, 1)) +
+                this->gates.slice(this->i_offset(), this->gate_extent()) *
+                this->gates.slice(this->c_offset(), this->gate_extent());
+
+        h.chip(this->time_step, 1).device(device) =
+                this->gates.slice(this->o_offset(), this->gate_extent()) *
+                RecurrentActivation::Activate(cs.chip(this->time_step, 1));
+    }
+
+
+    template<typename Device>
+    void Backward(const Tensor<3> &gradients, const Tensor<3> &x,
+                  const Tensor<2> &w, Tensor<2> &dL_dw,
+                  const Tensor<3> &h, const Tensor<3> &c,
+                  const Device &device = Eigen::DefaultDevice())
+    {
+        if (this->time_step != 0) {
+            // calculate dL_dh_prev, dL_dc_prev
+            this->dL_dh_prev.resize();
+            this->dL_dc_prev.resize();
+        }
     }
 
 
 protected:
-    inline Dims<3> x_offset() const
-    { return Dims<3>(0, this->time_step, 0); }
+    inline Dims<2> i_offset()
+    { return Dims<2>(0, 0); }
 
 
-    inline Dims<3> x_extent() const
-    { return Dims<3>(this->batch_size, 1, this->input_len); }
+    inline Dims<2> f_offset()
+    { return Dims<2>(0, this->output_len); }
 
 
-    inline Dims<3> h_prev_offset() const
-    { return Dims<3>(0, this->time_step, 0); }
+    inline Dims<2> c_offset()
+    { return Dims<2>(0, this->output_len * 2); }
 
 
-    inline Dims<3> h_extent() const
-    { return Dims<3>(this->batch_size, 1, this->output_len); }
+    inline Dims<2> o_offset()
+    { return Dims<2>(0, this->output_len * 3); }
+
+
+    inline Dims<2> gate_extent()
+    { return Dims<2>(this->gates.dimension(0), this->output_len); }
+
+
+    // W weights offsets, extents
+    inline Dims<2> wi_offset()
+    { return Dims<2>(0, 0); }
+
+
+    inline Dims<2> wf_offset()
+    { return Dims<2>(0, this->output_len * 1); }
+
+
+    inline Dims<2> wc_offset()
+    { return Dims<2>(0, this->output_len * 2); }
+
+
+    inline Dims<2> wo_offset()
+    { return Dims<2>(0, this->output_len * 3); }
+
+
+    inline Dims<2> w_extent()
+    { return Dims<2>(this->input_len, this->output_len); }
+
+
+    // U weights offsets, extents
+    inline Dims<2> ui_offset()
+    { return Dims<2>(this->input_len, 0); }
+
+
+    inline Dims<2> uf_offset()
+    { return Dims<2>(this->input_len, this->output_len); }
+
+
+    inline Dims<2> uc_offset()
+    { return Dims<2>(this->input_len, this->output_len * 2); }
+
+
+    inline Dims<2> uo_offset()
+    { return Dims<2>(this->input_len, this->output_len * 3); }
+
+
+    inline Dims<2> u_extent()
+    { return Dims<2>(this->output_len, this->output_len); }
 
 
 protected:
     const Eigen::Index time_step;
-    Eigen::Index batch_size = -1; // set during forward propagation
     const Eigen::Index input_len;
     const Eigen::Index output_len;
+    Eigen::Index batch_size = -1;
 
-    Tensor<3> gates;
+    Tensor<2> x_h_prev;
+
+    Tensor<2> gates;
+    Tensor<2> p_gates;
+
+    Tensor<2> dL_dh_prev;
+    Tensor<2> dL_dc_prev;
 
 };
 
--- flare/main.cpp
+++ flare/main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include <chrono>
 #include <flare/flare.hpp>
-#include "examples/learn_addition.hpp"
 
 
 void test()
 {
-    LearnAddition();
+    Eigen::ThreadPoolDevice device = Eigen::ThreadPoolDevice(
+            new Eigen::ThreadPool(8), 2);
+
+    Eigen::Index batch_size = 1;
+    Eigen::Index input_len = 3;
+    Eigen::Index output_len = 5;
+    Eigen::Index time_sequence = 5;
+
+    fl::Tensor<2> w(input_len + output_len, output_len * 4);
+    w.setConstant(1.0);
+
+    fl::Tensor<3> x(batch_size, time_sequence, input_len);
+    x.setConstant(1.0);
+    fl::Tensor<3> h(batch_size, time_sequence, output_len);
+    fl::Tensor<3> cs(batch_size, time_sequence, output_len);
+    fl::Tensor<3> y(batch_size, time_sequence, output_len);
+    y.setConstant(1.0);
+        
+    std::vector<fl::LSTMCell<fl::Linear, fl::Linear>> cells;
+    cells.reserve(time_sequence);
+
+    for (int i = 0; i < time_sequence; i++) {
+        cells.emplace_back(i, input_len, output_len);
+    }
+
+    for (int i = 0; i < time_sequence; i++) {
+        cells[i].Forward(x, w, h, cs, device);
+    }
+
+    std::cout << "h output: " << h.dimensions() << "\n" << h << "\n";
+
 }
 
 
